2ce4e869cf27f22b62ce2b72457c32d49d6b5fde
Patrick Bucher <patrick.bucher@mailbox.org>
2026-07-28 20:50:08 +0200

    100% test coverage

--- throttle_test.go
+++ throttle_test.go
@@ -22,7 +22,6 @@ func TestInitialRequestServedImmediately(t *testing.T) {
 func TestRequestDelayedForEntity(t *testing.T) {
 	entity, valueA, valueB := "somebody", "something", "other thing"
 	th := New[string, string](200*time.Millisecond, 500*time.Millisecond)
-	minimumWaitTime := 500 * time.Millisecond
 
 	resultA, err := th.Await(entity, &valueA, 100*time.Millisecond)
 	if err != nil {
@@ -32,7 +31,7 @@ func TestRequestDelayedForEntity(t *testing.T) {
 		t.Error("wrong result A")
 	}
 
-	resultB, err := th.Await(entity, &valueB, 2*minimumWaitTime)
+	resultB, err := th.Await(entity, &valueB, time.Second)
 	if err != nil {
 		t.Error("timeout B")
 	}
@@ -40,7 +39,35 @@ func TestRequestDelayedForEntity(t *testing.T) {
 		t.Error("wrong result B")
 	}
 
-	_, err = th.Await(entity, &valueB, minimumWaitTime-100*time.Millisecond)
+	// timeout < entity spawn interval
+	_, err = th.Await(entity, &valueB, 400*time.Millisecond)
+	if !errors.Is(err, TimeoutError) {
+		t.Errorf("expected timeout, but succeeded")
+	}
+}
+
+func TestRequestDelayedGlobally(t *testing.T) {
+	entity, valueA, valueB := "somebody", "something", "other thing"
+	th := New[string, string](500*time.Millisecond, 200*time.Millisecond)
+
+	resultA, err := th.Await(entity, &valueA, 100*time.Millisecond)
+	if err != nil {
+		t.Error("timeout A")
+	}
+	if resultA != &valueA {
+		t.Error("wrong result A")
+	}
+
+	resultB, err := th.Await(entity, &valueB, time.Second)
+	if err != nil {
+		t.Error("timeout B")
+	}
+	if resultB != &valueB {
+		t.Error("wrong result B")
+	}
+
+	// timeout < global spawn interval
+	_, err = th.Await(entity, &valueB, 400*time.Millisecond)
 	if !errors.Is(err, TimeoutError) {
 		t.Errorf("expected timeout, but succeeded")
 	}
@@ -65,5 +92,24 @@ func TestSecondEntityDelayedGlobally(t *testing.T) {
 	}
 	if resultB != &valueB {
 		t.Error("wrong result B")
+	}
+}
+
+func TestSecondEntityDelayedForEntity(t *testing.T) {
+	entityA, entityB := "Alice", "Bob"
+	valueA, valueB := "Apple", "Pear"
+	th := New[string, string](time.Second, 3*time.Second)
+
+	resultA, err := th.Await(entityA, &valueA, 100*time.Millisecond)
+	if err != nil {
+		t.Error("timeout A")
+	}
+	if resultA != &valueA {
+		t.Error("wrong result A")
+	}
+
+	_, err = th.Await(entityB, &valueB, time.Second-100*time.Millisecond)
+	if !errors.Is(err, TimeoutError) {
+		t.Errorf("expected timeout, but succeeded")
 	}
 }
