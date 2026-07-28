ab98d6da552bfb8ade23c6332c482792f6e41f27
Patrick Bucher <patrick.bucher@mailbox.org>
2026-07-28 13:23:26 +0200

    initial, untested implementation of throttling

--- throttle.go
+++ throttle.go
@@ -1,1 +1,68 @@
 package throttle
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+var TimeoutError error
+
+func init() {
+	TimeoutError = errors.New("timeout")
+}
+
+// Throttle allows to throttles the usage of values of type V identified by entities of type T.
+type Throttle[T comparable, V any] struct {
+	// GlobalSpawn is a channel that spawns tokens at GlobalRate.
+	GlobalSpawn *time.Ticker
+
+	// EntitySpawnMux is a mutex over EntitySpawn.
+	EntitySpawnMux sync.RWMutex
+
+	// EntitySpawn maps entities to their individual channel that spawns tokens at EntityRate.
+	EntitySpawn map[T]*time.Ticker
+
+	// EntityRate is the rate at which entity-specific tokens are spanwed.
+	EntityRate time.Duration
+}
+
+// New creates a new Throttle by defining its global and entity token spawn rate.
+func New[T comparable, V any](globalRate, entityRate time.Duration) *Throttle[T, V] {
+	return &Throttle[T, V]{
+		GlobalSpawn: time.NewTicker(globalRate),
+		EntitySpawn: make(map[T]*time.Ticker),
+		EntityRate:  entityRate,
+	}
+}
+
+// Await waits for the throttle object to spawn both a token for the individual
+// entity and globally. If those tokens are spawned within the given timeout,
+// the given value is returned back. Otherwise, TimeoutError is returned.
+func (t *Throttle[T, V]) Await(entity T, value *V, timeout time.Duration) (*V, error) {
+	timeoutChan := time.NewTimer(timeout)
+
+	t.EntitySpawnMux.RLocker().Lock()
+	var entityTicker *time.Ticker
+	entityTicker, ok := t.EntitySpawn[entity]
+	t.EntitySpawnMux.Unlock()
+	if !ok {
+		entityTicker = time.NewTicker(t.EntityRate)
+		t.EntitySpawnMux.Lock()
+		t.EntitySpawn[entity] = entityTicker
+		t.EntitySpawnMux.Unlock()
+	}
+
+	select {
+	case <-timeoutChan.C:
+		return nil, TimeoutError
+	case <-entityTicker.C:
+		// process to next step
+	}
+	select {
+	case <-timeoutChan.C:
+		return nil, TimeoutError
+	case <-t.GlobalSpawn.C:
+		return value, nil
+	}
+}
